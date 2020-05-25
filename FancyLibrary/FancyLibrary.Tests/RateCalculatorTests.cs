ec6f9b08b995de78c0a3402550462ebf856e3db1
Sergey-Nosov-GH <sergey.nosov@orderfactory.com>
2020-05-24 22:29:43 -0700

    implement fizbuz

---
+++ FancyLibrary/FancyLibrary.Tests/RateCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FancyLibrary.Tests
+{
+    public class RateCalculatorTests
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(199, "199")]
+        [InlineData(3, "Fiz")]
+        [InlineData(3 * 193 * 197, "Fiz")]
+        [InlineData(5, "Buz")]
+        [InlineData(5 * 193 * 197 * 199, "Buz")]
+        [InlineData(15, "FizBuz")]
+        [InlineData(15 * 1024 * 1024, "FizBuz")]
+        [InlineData(-15 * 1024 * 1024, "FizBuz")]
+        public void GivenAnIntegerWhenFizBuzThenProduceString(int input, string expected)
+        {
+            var calculator = new RateCalculator();
+
+            var output = calculator.FizBuz(input);
+
+            output.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GivenThreeWhenFizBuzThenFiz()
+        {
+            //AAA
+
+            //Arrange
+            var calculator = new RateCalculator();
+            const int input = 3;
+
+            //Act
+            var output = calculator.FizBuz(input);
+
+            //Assert
+            output.Should().Be("Fiz");
+        }
+    }
+}
---
+++ FancyLibrary/FancyLibrary/RateCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace FancyLibrary
+{
+    public interface IRateCalculator
+    {
+        string FizBuz(int i);
+    }
+
+    public class RateCalculator : IRateCalculator
+    {
+        public string FizBuz(int i)
+        {
+            var divisibleByThree = DivisibleBy(i, 3);
+            var divisibleByFive = DivisibleBy(i, 5);
+
+            if (divisibleByThree && divisibleByFive) return "FizBuz";
+            if (divisibleByThree) return "Fiz";
+            return divisibleByFive ? "Buz" : i.ToString();
+        }
+
+        private static bool DivisibleBy(int number, int divisor)
+        {
+            return number % divisor == 0;
+        }
+    }
+}
